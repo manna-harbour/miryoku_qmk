3ae67f3e2ae6a12ab7270701aff01818b2ddb723
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2020-05-26 14:30:42 +1000

    Add alt. alphas, add MBO, change button order, remove crkbd h/w

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -1,5 +1,9 @@
 # generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
 
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
-LTO_ENABLE = yes
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+
+
+ifneq ($(strip $(MIRYOKU_BASE)),)
+  OPT_DEFS += -DMIRYOKU_BASE_$(MIRYOKU_BASE)
+endif
