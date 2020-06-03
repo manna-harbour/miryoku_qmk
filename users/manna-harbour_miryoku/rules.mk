b1b79b67baa7f4a7cbad3a0a154dd385f892489f
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2020-06-03 20:46:05 +1000

    Build keymap as object instead of including .c file
    - Add kle subset mapping images
    - Add alternative mapping support
    - Add alternative mapping for ortho_4x12
    - Add contact section

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -1,9 +1,16 @@
 # generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
 
-MOUSEKEY_ENABLE = yes        # Mouse keys
-EXTRAKEY_ENABLE = yes        # Audio control and System control
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
 
+SRC += manna-harbour_miryoku.c # keymap
 
+# select alternative base layer alphas
 ifneq ($(strip $(MIRYOKU_BASE)),)
   OPT_DEFS += -DMIRYOKU_BASE_$(MIRYOKU_BASE)
+endif
+
+# select alternative subset mappings
+ifneq ($(strip $(MIRYOKU_LAYOUT)),)
+  OPT_DEFS += -DMIRYOKU_LAYOUT_$(MIRYOKU_LAYOUT)
 endif
