6c14f0e98965076047cfd8e3e29d3d4df1763d99
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2020-08-07 14:21:24 +1000

    Add planck_mit layout
    Remove alternative bottom row support from ortho_4x12

--- layouts/community/ortho_4x12/manna-harbour_miryoku/config.h
+++ layouts/community/ortho_4x12/manna-harbour_miryoku/config.h
@@ -26,6 +26,6 @@ LAYOUT_ortho_4x12(\
 K00,   K01,   K02,   K03,   K04,   KC_NO, KC_NO, K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,   KC_NO, KC_NO, K15,   K16,   K17,   K18,   K19,\
 K20,   K21,   K22,   K23,   K24,   KC_NO, KC_NO, K25,   K26,   K27,   K28,   K29,\
-KC_NO, KC_NO, K32,   K33,   K34,   K34,   K35,   K35,   K36,   K37,   KC_NO, KC_NO\
+KC_NO, KC_NO, K32,   K33,   K34,   KC_NO, KC_NO, K35,   K36,   K37,   KC_NO, KC_NO\
 )
 #endif
