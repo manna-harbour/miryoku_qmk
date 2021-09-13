7fffb88ca6bf638cb578df907dba6635bd97445a
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-09-13 16:15:14 +1000

    [miryoku] Add MIRYOKU_MAPPING=LITE to 60_ansi

--- layouts/community/60_ansi/manna-harbour_miryoku/config.h
+++ layouts/community/60_ansi/manna-harbour_miryoku/config.h
@@ -9,7 +9,7 @@
 #define XXX KC_NO
 
 #if defined MIRYOKU_MAPPING_NOREVERSEANGLE
-#define LAYOUT_miryoku(\
+  #define LAYOUT_miryoku(\
 K00,  K01,  K02,  K03,  K04,                    K05,  K06,  K07,  K08,  K09,\
 K10,  K11,  K12,  K13,  K14,                    K15,  K16,  K17,  K18,  K19,\
 K20,  K21,  K22,  K23,  K24,                    K25,  K26,  K27,  K28,  K29,\
@@ -22,8 +22,38 @@ K00,  K01,  K02,  K03,  K04,  XXX,  XXX,  XXX,  K05,  K06,  K07,  K08,  K09,  XX
   XXX,        K32,  K33,  K34,  XXX,  XXX,  XXX,  XXX,  K35,  K36,  K37,  XXX,\
   XXX,  XXX,  XXX,              XXX,              XXX,  XXX,        XXX,  XXX\
 )
+#elif defined MIRYOKU_MAPPING_LITE
+  #if defined MIRYOKU_LAYERS_FLIP
+    #define LAYOUT_miryoku( \
+         K00,     K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09, \
+         K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19, \
+         K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29, \
+         N30,     N31,     K32,     K33,     K34,     K35,     K36,     K37,     N38,     N39 \
+) \
+LAYOUT_60_ansi( \
+KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+KC_TAB,  K00,     K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09,     KC_LBRC, KC_RBRC, KC_BSLS, \
+KC_CAPS, K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19,     KC_SCLN, KC_ENT, \
+KC_LSFT, K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,     KC_RSFT, \
+KC_LCTL, KC_LGUI, KC_LALT,                            K36,                                KC_RALT, KC_RGUI, KC_APP,  KC_RCTL \
+)
+  #else
+    #define LAYOUT_miryoku( \
+         K00,     K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09, \
+         K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19, \
+         K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29, \
+         N30,     N31,     K32,     K33,     K34,     K35,     K36,     K37,     N38,     N39 \
+) \
+LAYOUT_60_ansi( \
+KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+KC_TAB,  K00,     K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09,     KC_LBRC, KC_RBRC, KC_BSLS, \
+KC_CAPS, K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19,     KC_SCLN, KC_ENT, \
+KC_LSFT, K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,     KC_RSFT, \
+KC_LCTL, KC_LGUI, KC_LALT,                            K33,                                         KC_RALT, KC_RGUI, KC_APP,  KC_RCTL \
+)
+  #endif
 #else
-#define LAYOUT_miryoku(\
+  #define LAYOUT_miryoku(\
             K00,  K01,  K02,  K03,  K04,        K05,  K06,  K07,  K08,  K09,\
             K10,  K11,  K12,  K13,  K14,        K15,  K16,  K17,  K18,  K19,\
             K20,  K21,  K22,  K23,  K24,        K25,  K26,  K27,  K28,  K29,\
