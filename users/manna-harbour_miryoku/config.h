15e86cf9a4f1dcc184f039252abee30683b9cf3f
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-10-17 22:29:22 +1100

    [miryoku] Miryoku repo reorganisation
    - Move docs to Miryoku
    - Move code generation of keymap data to Miryoku Babel
    - Remove code generation for other files
    - Remove generated line from headers

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -1,6 +1,5 @@
 // Copyright 2019 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-// generated -*- buffer-read-only: t -*-
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,9 +19,6 @@
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
 #define AUTO_SHIFT_NO_SETUP
-
-// Recommended for heavy chording.
-#define QMK_KEYS_PER_SCAN 4
 
 // Mouse key speed and acceleration.
 #undef MOUSEKEY_DELAY
--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -1,12 +1,11 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
-# generated -*- buffer-read-only: t -*-
 
 MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
 AUTO_SHIFT_ENABLE = yes # Auto Shift
 
-SRC += manna-harbour_miryoku.c # keymap
+SRC += manna-harbour_miryoku.c # keymaps
 
 # alternative layouts:
 
