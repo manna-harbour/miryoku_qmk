f456c9c9de1fa1ecf472b80f84286e1d8e281792
boekhocg <61322825+boekhocg@users.noreply.github.com>
2021-11-20 13:13:31 -0500

    Update manna-harbour_miryoku.c

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -20,16 +20,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BUTTON] = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_BUTTON)
 };
 
-
-void oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-    switch (get_highest_layer(layer_state)) {
-        case MIRYOKU_LAYER_BASE:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-}
