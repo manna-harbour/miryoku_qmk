60e499eef0074c148e388c36b1217d182aa2e6cc
boekhocg <61322825+boekhocg@users.noreply.github.com>
2021-11-20 12:48:12 -0500

    Update keymap.c

--- layouts/community/split_3x6_3/manna-harbour_miryoku/keymap.c
+++ layouts/community/split_3x6_3/manna-harbour_miryoku/keymap.c
@@ -2,3 +2,19 @@
 // https://github.com/manna-harbour/miryoku
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#ifdef OLED_ENABLE
+void oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case MIRYOKU_LAYER_BASE:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+}
+#endif
