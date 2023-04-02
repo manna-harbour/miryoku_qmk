7dc74563a653da29dfd21594c90dd24685a04146
Jenelle <14959158+jenelle0x01@users.noreply.github.com>
2023-04-02 17:21:37 +0800

    Update custom_config.h
    Updated the config to use substitute instead of add layers

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,25 +5,13 @@
 
 #pragma once
 
-#define MIRYOKU_ALTERNATIVES_BASE_ISRT_FLIP \
-KC_Y,              KC_C,              KC_L,              KC_M,              KC_K,              KC_Z,              KC_F,              KC_U,              KC_COMM,           KC_QUOT,           \
-LGUI_T(KC_I),      LALT_T(KC_S),      LCTL_T(KC_R),      LSFT_T(KC_T),      KC_G,              KC_P,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_A),      LGUI_T(KC_O),      \
-LT(U_BUTTON,KC_Q), ALGR_T(KC_V),      KC_W,              KC_D,              KC_J,              KC_B,              KC_H,              KC_SLSH,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_X), \
-U_NP,              U_NP,              LT(U_FUN,KC_DEL),  LT(U_NUM,KC_BSPC), LT(U_SYM,KC_ENT),  LT(U_MOUSE,KC_TAB),LT(U_NAV,KC_SPC),  LT(U_MEDIA,KC_ESC),U_NP,              U_NP
-
-#define MIRYOKU_ALTERNATIVES_BASE_ISRT \
+#define MIRYOKU_LAYER_BASE \
 KC_Y,              KC_C,              KC_L,              KC_M,              KC_K,              KC_Z,              KC_F,              KC_U,              KC_COMM,           KC_QUOT,           \
 LGUI_T(KC_I),      LALT_T(KC_S),      LCTL_T(KC_R),      LSFT_T(KC_T),      KC_G,              KC_P,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_A),      LGUI_T(KC_O),      \
 LT(U_BUTTON,KC_Q), ALGR_T(KC_V),      KC_W,              KC_D,              KC_J,              KC_B,              KC_H,              KC_SLSH,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_X), \
 U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
 
-#define MIRYOKU_ALTERNATIVES_TAP_ISRT_FLIP \
-KC_Y,              KC_C,              KC_L,              KC_M,              KC_K,              KC_Z,              KC_F,              KC_U,              KC_COMM,           KC_QUOT,           \
-KC_I,              KC_S,              KC_R,              KC_T,              KC_G,              KC_P,              KC_N,              KC_E,              KC_A,              KC_O,              \
-KC_Q,              KC_V,              KC_W,              KC_D,              KC_J,              KC_B,              KC_H,              KC_SLSH,           KC_DOT,            KC_X,              \
-U_NP,              U_NP,              KC_DEL,            KC_BSPC,           KC_ENT,            KC_TAB,            KC_SPC,            KC_ESC,            U_NP,              U_NP
-
-#define MIRYOKU_ALTERNATIVES_TAP_ISRT \
+#define MIRYOKU_MIRYOKU_LAYER_TAP \
 KC_Y,              KC_C,              KC_L,              KC_M,              KC_K,              KC_Z,              KC_F,              KC_U,              KC_COMM,           KC_QUOT,           \
 KC_I,              KC_S,              KC_R,              KC_T,              KC_G,              KC_P,              KC_N,              KC_E,              KC_A,              KC_O,              \
 KC_Q,              KC_V,              KC_W,              KC_D,              KC_J,              KC_B,              KC_H,              KC_SLSH,           KC_DOT,            KC_X,              \
