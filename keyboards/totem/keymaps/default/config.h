b06516b40365d2eef425bfe8ab0e03386bcdd22d
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2023-05-01 12:47:36 +1000

    [keyboard] Add totem
    From https://github.com/GEIGEIGEIST/qmk-config-totem/tree/6f441966754d98ed0f87abfb35d282fa46151f3d/totem

---
+++ keyboards/totem/keymaps/default/config.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+
+
+//#define TAPPING_TERM 170
+#define TAPPING_TERM 170
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+
