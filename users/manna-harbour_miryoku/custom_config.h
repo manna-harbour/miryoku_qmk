247980e28272122dbee20fc23e1642afa01bea4b
C8DevMec <93236947+C8DevMec@users.noreply.github.com>
2023-09-17 17:29:33 +0800

    test remapping of layers

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -2,6 +2,13 @@
 // https://github.com/manna-harbour/miryoku
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+// TODO: 
+// TODO: Change mapping for 
 #pragma once
 
+
+#define MIRYOKU_LAYERMAPPING_BASE_QWERTY \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
+KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ESC),  LT(U_FUN,KC_SPC),LT(U_NUM,KC_ENT),  LT(U_NAV,KC_BSPC),   KC_ENT,            U_NP,              U_NP
