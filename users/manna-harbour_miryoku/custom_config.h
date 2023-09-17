b1187d989ae06a59738c07aece5428c24f5b31b5
C8DevMec <93236947+C8DevMec@users.noreply.github.com>
2023-09-17 18:08:39 +0800

    remap qwerty

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -6,7 +6,7 @@
 // TODO: Change mapping for
 
 
-#define MIRYOKU_LAYER_BASE_QWERTY \
+#define MIRYOKU_LAYERMAPPING_BASE_QWERTY \
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
 LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
 KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
