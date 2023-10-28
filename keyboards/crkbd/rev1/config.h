159aec8505c30525e96d66df8bb146acea5b7c70
regicidal.plutophage <36969337+regicidalplutophage@users.noreply.github.com>
2023-10-29 00:35:24 +0300

    Update config.h

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* ws2812 RGB LED */
-#define RGB_DI_PIN      TX
+#define RGB_DI_PIN      GP0
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLED_NUM 54 // Number of LEDs
