5a3df073048236329ddb62200156b3fdb91aba62
petridish-dg <74042416+petridish-dg@users.noreply.github.com>
2024-01-23 21:58:39 -0600

    Create config.h

---
+++ keyboards/peterkb/config.h
@@ -0,0 +1,26 @@
+// Copyright 2024 Peter de Guzman (@petridish-dg)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
+
+/* disable action features */
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
+
+#define MASTER_LEFT
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
