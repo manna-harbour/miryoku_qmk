a9c5e300e06d50bfed5f6fbcd1ac785ce32cfea5
Alexandre Andrade <aleandrade@gmail.com>
2025-02-07 20:48:51 +1100

    feat: 🎸 Define combo for Gnome shortcut

---
+++ keyboards/beekeeb/piantor/keymaps/manna-harbour_miryoku/keymap.c
@@ -0,0 +1,10 @@
+// Copyright 2019 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM restore_window_size[] = {RWIN(KC_LEFT), RWIN(KC_RIGHT), COMBO_END};
+combo_t key_combos[] = {
+    COMBO(restore_window_size, RWIN(KC_UP)),
+};
