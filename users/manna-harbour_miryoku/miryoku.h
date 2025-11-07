996d27510cd73e249ee3a3ddf69ef9d4a29a7944
yemik <oduyemik@outlook.com>
2025-11-07 18:03:17 +0000

    Update miryoku.h

--- users/manna-harbour_miryoku/miryoku.h
+++ users/manna-harbour_miryoku/miryoku.h
@@ -1,2 +1,5 @@
 #pragma once
+#include "miryoku_babel/miryoku_layer_list.h"
+#include "miryoku_babel/miryoku_layer_selection.h"
+#include "miryoku_babel/miryoku_layer_alternatives.h"
 #include "manna-harbour_miryoku.h"
