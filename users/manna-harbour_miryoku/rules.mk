45f96e29a70cc20816a09e69a98eaafc0dc55c40
yemik <oduyemik@outlook.com>
2025-11-07 18:04:32 +0000

    Update rules.mk

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -8,7 +8,7 @@ TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 
-SRC += $(USER_PATH)/manna-harbour_miryoku/manna-harbour_miryoku.c
+INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
 
 include users/manna-harbour_miryoku/custom_rules.mk
 
