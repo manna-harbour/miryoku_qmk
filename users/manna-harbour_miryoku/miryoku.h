f5cc7636d3cfe3ed17cec17e391157dad2bf1575
yemik <oduyemik@outlook.com>
2025-11-07 18:17:23 +0000

    Delete users/manna-harbour_miryoku/miryoku.h

--- users/manna-harbour_miryoku/miryoku.h
+++
@@ -1,5 +0,0 @@
-
-#include "miryoku_babel/miryoku_layer_list.h"
-#include "miryoku_babel/miryoku_layer_selection.h"
-#include "miryoku_babel/miryoku_layer_alternatives.h"
-#include "manna-harbour_miryoku.h"
