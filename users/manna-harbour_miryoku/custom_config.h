afce39c6d2ba520418f67ee84f3d903b82db2f84
yemik <oduyemik@outlook.com>
2025-11-07 19:05:05 +0000

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,145 @@
 
 #pragma once
 
+#define XXX KC_NO
+
+#define MIRYOKU_MAPPING( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+XXX,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  XXX, \
+XXX,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  XXX, \
+XXX,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  XXX , \
+                  K32,  K33,  K34,         K35,  K36,  K37 \
+)
+
+#define MIRYOKU_LAYER_NUM \
+KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),   \
+ TD(U_TD_BOOT),     \
+KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,            U_NA,              KC_RSFT,           KC_RCTL,           KC_RALT,          \
+ KC_RGUI,           \
+KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,          \
+ U_NA,              \
+U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,             \
+ U_NP
+
+#define MIRYOKU_LAYER_SYM \
+KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),   \
+ TD(U_TD_BOOT),     \
+KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           U_NA,              KC_RSFT,           KC_RCTL,           KC_RALT,          \
+ KC_RGUI,           \
+KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,          \
+ U_NA,              \
+U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,             \
+ U_NP
+
+#define MIRYOKU_LAYER_BUTTON \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,            \
+ U_UND,             \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NU,              U_NU,              KC_RSFT,           KC_RCTL,           KC_RALT,          \
+ KC_RGUI,           \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,            \
+ U_UND,             \
+U_NP,              U_NP,              KC_BTN3,           KC_BTN1,           KC_BTN2,           KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,             \
+ U_NP
+
+#define MIRYOKU_LAYER_FUN \
+KC_F12,            KC_F7,             KC_F8,             KC_F9,             KC_PSCR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),   \
+ TD(U_TD_BOOT),     \
+KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,           U_NA,              KC_RSFT,           KC_RCTL,           KC_RALT,          \
+ KC_RGUI,           \
+KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS,           U_NA,              TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  KC_ALGR,          \
+ U_NA,              \
+U_NP,              U_NP,              KC_APP,            KC_SPC,            KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,             \
+ U_NP
+
+#define MIRYOKU_LAYER_BASE \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,             \
+ KC_P,              \
+LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              RSFT_T(KC_J),      RCTL_T(KC_K),      RALT_T(KC_L),     \
+ RGUI_T(KC_QUOT),   \
+LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),   \
+ LT(U_BUTTON,KC_SLSH),\
+U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,             \
+ U_NP
+
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN      D3
+
+#ifdef RGBLIGHT_ENABLE
+
+#   define RGBLIGHT_EFFECT_BREATHING
+//#   define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#   define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#   define RGBLIGHT_EFFECT_SNAKE
+#   define RGBLIGHT_EFFECT_KNIGHT
+//#   define RGBLIGHT_EFFECT_CHRISTMAS
+#   define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#   define RGBLIGHT_EFFECT_RGB_TEST
+//#   define RGBLIGHT_EFFECT_ALTERNATING
+//#   define RGBLIGHT_EFFECT_TWINKLE
+
+#   define RGBLED_NUM 54
+#   define RGBLED_SPLIT \
+        { 27, 27 }
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#    define RGBLED_NUM       54 // Number of LEDs
+#    define RGB_MATRIX_LED_COUNT RGBLED_NUM
+#    define RGB_MATRIX_SPLIT \
+        { 27, 27 }
+#    define SPLIT_TRANSPORT_MIRROR
+
+#   define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#   define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#   define ENABLE_RGB_MATRIX_BREATHING
+#   define ENABLE_RGB_MATRIX_BAND_SAT
+#   define ENABLE_RGB_MATRIX_BAND_VAL
+#   define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#   define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#   define ENABLE_RGB_MATRIX_CYCLE_ALL
+#   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#   define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#   define ENABLE_RGB_MATRIX_RAINDROPS
+#   define ENABLE_RGB_MATRIX_HUE_BREATHING
+#   define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+#   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#   define ENABLE_RGB_MATRIX_SPLASH
+#   define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#endif
+
+// Shamelessly stolen mouse key settings from @pierrechevalier83 //
+// Set the mouse settings to a comfortable speed/accuracy trade-off
+// Assume the screen refresh rate is 60 Hz or higher
+// The default is 50. This makes the mouse ~3 times faster and more accurate
+// #undef to override default settings
+// #undefs MOUSEKEY_INTERVAL
+// #define MOUSEKEY_INTERVAL 16
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+// #undef MOUSEKEY_TIME_TO_MAX
+// #define MOUSEKEY_TIME_TO_MAX 40
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+/* #undef MOUSEKEY_DELAY */
+// #define MOUSEKEY_DELAY 100
+
+#define TAPPING_TERM_PER_KEY
+
+#define DIODE_DIRECTION COL2ROW
+
+#define SPLIT_USB_DETECT
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_MAC
